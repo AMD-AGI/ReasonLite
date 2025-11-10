9c23c5543ab59b7f374dac7f4e28042c74de9cc6
ChushiChen <Chushi.Chen@amd.com>
2025-11-10 02:34:20 +0000

    minor fix

--- eval/eval.sh
+++ eval/eval.sh
@@ -5,7 +5,7 @@ export VLLM_USE_AITER_UNIFIED_ATTENTION=1
 export VLLM_ROCM_USE_AITER_MHA=0
 
 VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 VLLM_USE_V1=1 VLLM_WORKER_MULTIPROC_METHOD=spawn python3 uni_eval.py \
-    --base_model /home/Chushi.Chen@amd.com/dev/checkpoints/ERM-ccs-1105 \
+    --base_model <path_to_your_checkpoint> \
     --chat_template_name default \
     --system_prompt_name disabled \
     --output_dir results \
